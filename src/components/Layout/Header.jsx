373204d1a0396299a7f1a60367d6edc8ea58bb22
yats1304 <chaubaly@gmail.com>
2025-08-23 16:25:39 +0530

    NavBar completed

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,11 +1,54 @@
-import { AppBar, Box, Typography } from '@mui/material'
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+import "../../styles/HeaderStyles.css"
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 
 const Header = () => {
+ const [mobileOpen, setMobileOpen] = useState(false);
+
+ //Menu Drawer
+
   return (
     <div>
         <Box>
-          <AppBar>
-            <Typography>My Resturant</Typography>
+          <AppBar component={"nav"} sx={{bgcolor:"black"}}>
+            <Toolbar className="navbar">
+              <IconButton>
+                <MenuIcon 
+                color="inherit" 
+                arial-label="open drawer" 
+                edge= "start" 
+                sx={{
+                  mr:2,
+                  display: { sm: "none" },
+                  color: "white"
+                }}
+                />
+              </IconButton>
+              <Typography
+                component="div"
+                sx={{ flexGrow: 1 }}
+              >
+                <img src="/logo_tt.png" alt="Logo" width={180} height={150}/>
+              </Typography>
+              <Box sx={{display: {xs: "none", sm: "block"} }}>
+                <ul className='navigation-menu'>
+                  <li>
+                    <Link to={'/'}>Home</Link>
+                  </li>
+                  <li>
+                    <Link to={'/menu'}>Menu</Link>
+                  </li>
+                  <li>
+                    <Link to={'/about'}>About</Link>
+                  </li>
+                  <li>
+                    <Link to={'/contact'}>Contact</Link>
+                  </li>
+                </ul>
+              </Box>
+            </Toolbar>
           </AppBar>
         </Box>
     </div>
